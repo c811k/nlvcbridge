badd1cdd8f7a0b8a5ff5fda660b7f059206efdb2
Caleb Kang <bhk811@gmail.com>
2021-02-21 23:39:48 -0800

    weekly updates

--- js/timer.js
+++ js/timer.js
@@ -1,4 +1,4 @@
-var deadline = "December 13 2020 11:25:00 GMT-0800";
+var deadline = "February 28 2021 11:25:00 GMT-0800";
 
 function getTimeRemaining(endtime) {
     var t = Date.parse(endtime) - Date.parse(new Date());
